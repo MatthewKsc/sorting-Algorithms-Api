1cc9414fe990b42230ad93470c35e47f5a034f77
MatthewKsc <ksciukmateusz@gmail.com>
2020-08-21 12:14:01 +0200

    Initial commit

---
+++ src/main/java/com/matthewksc/sortingAlgorithmsApi/SortingAlgorithmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.matthewksc.sortingAlgorithmsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortingAlgorithmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortingAlgorithmsApiApplication.class, args);
+	}
+
+}
