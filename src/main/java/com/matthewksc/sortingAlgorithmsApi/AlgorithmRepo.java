99b58f83f5dcd8bc1efab2be789d771e232e4029
MatthewKsc <ksciukmateusz@gmail.com>
2020-08-22 13:27:31 +0200

    Correct mistake

--- src/main/java/com/matthewksc/sortingAlgorithmsApi/AlgorithmRepo.java
+++ src/main/java/com/matthewksc/sortingAlgorithmsApi/AlgorithmRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AlgorithmRepo extends JpaRepository<Long, Algorithm> {
+public interface AlgorithmRepo extends JpaRepository<Algorithm, Long> {
 }
