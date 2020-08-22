f74479cff585d4c344f7c2cc8cbe475af8be9eb8
MatthewKsc <ksciukmateusz@gmail.com>
2020-08-22 13:29:51 +0200

    Service for algorithm entity

---
+++ src/main/java/com/matthewksc/sortingAlgorithmsApi/AlgorithmService.java
@@ -0,0 +1,32 @@
+package com.matthewksc.sortingAlgorithmsApi;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AlgorithmService {
+
+    private AlgorithmRepo algorithmRepo;
+
+    public AlgorithmService(AlgorithmRepo algorithmRepo) {
+        this.algorithmRepo = algorithmRepo;
+    }
+
+    public List<Algorithm> findAll() {
+        return algorithmRepo.findAll();
+    }
+
+    public <S extends Algorithm> List<S> saveAll(Iterable<S> iterable) {
+        return algorithmRepo.saveAll(iterable);
+    }
+
+    public <S extends Algorithm> S save(S s) {
+        return algorithmRepo.save(s);
+    }
+
+    public Optional<Algorithm> findById(Long aLong) {
+        return algorithmRepo.findById(aLong);
+    }
+}
