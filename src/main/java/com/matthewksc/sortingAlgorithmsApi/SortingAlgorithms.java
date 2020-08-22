ec9d34eed75a3d8be3a28690f392645b12bf425a
MatthewKsc <ksciukmateusz@gmail.com>
2020-08-22 13:51:43 +0200

    Init of Sorting Class

---
+++ src/main/java/com/matthewksc/sortingAlgorithmsApi/SortingAlgorithms.java
@@ -0,0 +1,15 @@
+package com.matthewksc.sortingAlgorithmsApi;
+
+public class SortingAlgorithms {
+
+    public Integer[] bubbleSort(int[] arr){
+    }
+    public Integer[] selectionSort(int[] arr){
+    }
+    public Integer[] insertSort(int[] arr){
+    }
+    public Integer[] quicksort(int arr[], int left, int right){
+    }
+    public Integer[] mergeSort(int arr[],int lo, int n){
+    }
+}
