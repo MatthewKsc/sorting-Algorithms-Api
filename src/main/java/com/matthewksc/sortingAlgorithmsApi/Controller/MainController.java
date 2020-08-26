da573fc5ec4ebdc40afee7fc13779263057f10e8
MatthewKsc <ksciukmateusz@gmail.com>
2020-08-26 15:50:04 +0200

    Added ResponseEntity impl to setup proper header for to frontend connection

--- src/main/java/com/matthewksc/sortingAlgorithmsApi/Controller/MainController.java
+++ src/main/java/com/matthewksc/sortingAlgorithmsApi/Controller/MainController.java
@@ -3,8 +3,11 @@ package com.matthewksc.sortingAlgorithmsApi.Controller;
 
 import com.matthewksc.sortingAlgorithmsApi.Services.AlgorithmService;
 import com.matthewksc.sortingAlgorithmsApi.dao.Algorithm;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class MainController {
@@ -16,7 +19,17 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public Iterable<Algorithm> getAllResults(){
-        return algorithmService.findAll();
+    public ResponseEntity<List<Algorithm>> getAllResults() {
+
+        /*for javascript connection we need to add cors headers,
+        and for that we need to use responseEntity to setup
+        proper header to get connection
+        */
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Allow-Origin",
+                "http://127.0.0.1:5500"); //headerValue set up to your front application
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(algorithmService.findAll());
     }
 }
